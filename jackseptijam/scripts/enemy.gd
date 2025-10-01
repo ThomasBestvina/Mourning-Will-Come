637aeed6bc46f3cb3cdf68fc658855a4bf1c4910
Thomas Bestvina <bestvinathomas@gmail.com>
2025-10-01 13:50:00 -0600

    Enemies now die from towers

--- jackseptijam/scripts/enemy.gd
+++ jackseptijam/scripts/enemy.gd
@@ -3,12 +3,19 @@ class_name Enemy
 
 signal enemy_win
 
+signal died
+
 @export var speed = 2.0
+@export var health = 10.0
+
 
 @export var strength_score = 1.0
 
 func _process(delta: float) -> void:
 	set_progress(get_progress() + speed * delta)
+	if health <= 0:
+		emit_signal("died")
+		queue_free()
 	
 	if(get_progress_ratio() >= 0.99):
 		emit_signal("enemy_win")
--- jackseptijam/scripts/tower.gd
+++ jackseptijam/scripts/tower.gd
@@ -2,30 +2,48 @@ extends Node3D
 
 enum targetting_mode {STRONGEST, NEAREST, LAST, FIRST}
 
-@export var target_mode: targetting_mode
+@export var target_mode: targetting_mode = targetting_mode.NEAREST
 
 @export var range: float = 8
+
+@export var cooldown: float = 1.2
+@export var damage: float = 3.0
 
 var enemylist = []
 
 var target: Enemy
 
+var did_call: bool = false
+
 func _ready() -> void:
-	$target_area/CollisionShape3D.shape = $target_area/CollisionShape3D.shape.duplicate()
 	$RangeDisplayMesh.mesh = $RangeDisplayMesh.mesh.duplicate()
-	$target_area/CollisionShape3D.shape.radius = range
 	$RangeDisplayMesh.mesh.top_radius = range
 	$RangeDisplayMesh.mesh.bottom_radius = range
+	
+	await StoatStash.repeat_call(shoot, cooldown)
+	
 
 func _process(delta: float) -> void:
-	choose_target()
+	target = choose_target()
 	if target == null: return
 
+func shoot():
+	print(target)
+	if(target == null): return
+	target.health -= damage
+	print(target.health)
 
 func choose_target():
+	var possibilities = get_tree().get_nodes_in_group("enemy")
+	enemylist.clear()
+	for enemy: Enemy in possibilities:
+		if enemy.global_position.distance_to(global_position) < range:
+			enemylist.append(enemy)
+	
 	if(enemylist.is_empty()): 
 		target = null
 		return
+	
 	match target_mode:
 		targetting_mode.STRONGEST:
 			return get_strongest()
@@ -36,39 +54,30 @@ func choose_target():
 		targetting_mode.FIRST:
 			return get_first()
 
-func get_strongest():
+func get_strongest() -> Enemy:
 	var strongest: Enemy = enemylist[0]
 	for enemy: Enemy in enemylist:
 		if(enemy.strength_score > strongest.strength_score):
 			strongest = enemy
 	return strongest
 
-func get_nearest():
+func get_nearest() -> Enemy:
 	var nearest: Enemy = enemylist[0]
 	for enemy: Enemy in enemylist:
 		if(enemy.global_position.distance_squared_to(global_position) < enemy.global_position.distance_squared_to(global_position)):
 			nearest = enemy
 	return nearest
 
-func get_last():
+func get_last() -> Enemy:
 	var last: Enemy = enemylist[0]
 	for enemy: Enemy in enemylist:
 		if(enemy.progress_ratio < last.progress_ratio):
 			last = enemy
 	return last
 
-func get_first():
+func get_first() -> Enemy:
 	var first: Enemy = enemylist[0]
 	for enemy: Enemy in enemylist:
 		if(enemy.progress_ratio > first.progress_ratio):
 			first = enemy
 	return first
-
-func _on_target_area_body_entered(body: Node3D) -> void:
-	if(body is Enemy):
-		enemylist.append(body)
-
-
-func _on_target_area_body_exited(body: Node3D) -> void:
-	if(body is Enemy and enemylist.has(body)):
-		enemylist.erase(body)
